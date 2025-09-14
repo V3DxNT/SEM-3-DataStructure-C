872b9627cfab35b31739b25392f4e16acd71e675
V3DxNT <ved4geeks@gmail.com>
2025-09-14 12:11:31 +0530

    C 2 : Add Push Method

--- C2_InfixToPostfix.c
+++ C2_InfixToPostfix.c
@@ -8,3 +8,11 @@
 char STACK[MAX];
 int top = -1;
 
+void push(char ch) {
+    if (top == MAX - 1) {
+        printf("Stack OverFlow :is full\n");
+        return;
+    }
+    STACK[++top] = ch;
+}
+
