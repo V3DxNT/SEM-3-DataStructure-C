4a04f99a6ebec3e01ac03b86e8e8f0c129a42877
V3DxNT <ved4geeks@gmail.com>
2025-10-20 23:12:32 +0530

    ADD : Main StackOperatios

--- C5_PostfixEvaluation.c
+++ C5_PostfixEvaluation.c
@@ -1,6 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>  // For pow()
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
 
 int main() {
+    char postfix[MAX];
+    int result;
+
+    printf("Enter a postfix expression: ");
+    scanf("%s", postfix);
+
+    result = eval(postfix);
+
+    printf("Result is: %d\n", result);
     return 0;
 }
