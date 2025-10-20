16494b13fe2ffc706166cbee87afc6a143b1735a
V3DxNT <ved4geeks@gmail.com>
2025-10-20 22:40:52 +0530

    ADD : Precedence function

--- C4_InfixToPostfix.c
+++ C4_InfixToPostfix.c
@@ -31,3 +31,22 @@ char peek() {
     }
     return STACK[top];
 }
+
+//Function to return precedence of operator
+int precedence(char op) {
+    switch(op) {
+        case '^':
+            return 3;
+
+        case '*':
+        case '/':
+            return 2;
+
+        case '+':
+        case '-':
+            return 1;
+
+        default:
+            return 0;
+    }
+}
