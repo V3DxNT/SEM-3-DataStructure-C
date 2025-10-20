5ae553a6ccf227aa5c1b1398ba64542fa9d8be2b
V3DxNT <ved4geeks@gmail.com>
2025-10-20 23:13:44 +0530

    ADD : Stack Operations PUSH POP

--- C5_PostfixEvaluation.c
+++ C5_PostfixEvaluation.c
@@ -8,6 +8,25 @@
 int stack[MAX];
 int top = -1;
 
+// Push value onto stack
+void push(int val) {
+    if (top >= MAX - 1) {
+        printf("Stack Overflow\n");
+        exit(1);
+    }
+    stack[++top] = val;
+}
+
+// Pop value from stack
+int pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    return stack[top--];
+}
+
+
 int main() {
     char postfix[MAX];
     int result;
