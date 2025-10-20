d96bafbf8a9183d3669a41bc6a663be79e7aa66c
V3DxNT <ved4geeks@gmail.com>
2025-10-20 22:39:27 +0530

    ADD : Stack Operations PUSH POP PEEK

--- C4_InfixToPostfix.c
+++ C4_InfixToPostfix.c
@@ -7,3 +7,27 @@
 //Stack Implementation
 char STACK[MAX];
 int top = -1;
+
+void push(char ch) {
+    if (top == MAX - 1) {
+        printf("Stack OverFlow :is full\n");
+        return;
+    }
+    STACK[++top] = ch;
+}
+
+char pop() {
+    if (top == -1) {
+        printf("Stack UnderFlow : is empty\n");
+        return -1;
+    }
+    return STACK[top--];
+}
+
+char peek() {
+    if (top == -1) {
+        printf("Stack UnderFlow : is empty\n");
+        return -1;
+    }
+    return STACK[top];
+}
