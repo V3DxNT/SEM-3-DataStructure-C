77960ca8d09c4c3101477ed794f4fcfebb96a0a7
V3DxNT <ved4geeks@gmail.com>
2025-10-22 17:45:45 +0530

    Add Enqueue and Dequeue Fucntion

--- C7_CirQ_CallCentre.c
+++ C7_CirQ_CallCentre.c
@@ -19,3 +19,35 @@ int isEmpty() {
 int isFull() {
     return ((rear + 1) % MAX == front);
 }
+
+//Enqueue Operation
+void addCall(Call c) {
+    if (isFull()) {
+        printf("Queue Overflow\n");
+        printf("Cannot Add another call");
+        return;
+    }
+    if (isEmpty()) {
+        front=0;
+        rear=0;
+    }else {
+        rear = (rear + 1) % MAX;
+    }
+
+    queueArr[rear] = c;
+    printf("Added Call: Call ID %d Caller Name %s",c.callID,c.callerName);
+}
+
+void deleteCal() {
+    if (isEmpty()) {
+        printf("Queue Underflow\n");
+        return;
+    }
+    Call c = queueArr[front];
+    printf("Deleting Call Call ID %d Caller Name %s",c.callID,c.callerName);
+    if (front == rear) {
+        front=rear=-1;
+    }else {
+        front = (front + 1) % MAX;
+    }
+}
