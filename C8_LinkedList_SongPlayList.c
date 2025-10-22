9639f94c88cd3a2c4d89cfe5328d14dd670d56e2
V3DxNT <ved4geeks@gmail.com>
2025-10-22 18:22:35 +0530

    C8 : LinkedList - SongPlayList

--- C8_LinkedList_SongPlayList.c
+++ C8_LinkedList_SongPlayList.c
@@ -25,6 +25,8 @@ void addSong() {
     printf("Enter the Song Name\n");
     scanf("%s",name);
 
+    // scanf(" %[^\n]", name);   //For inputs even after a space
+
     Node* newNode = createNode(name);
     if (head==NULL) {
         head = newNode;
@@ -46,7 +48,7 @@ void playStart() {
     }
     Node* temp = head;
     printf("Playing Songs:\n");
-    while (temp->next!=NULL) {
+    while (temp!=NULL) {
         printf("%s\n",temp->song);
         temp = temp->next;
     }
@@ -96,8 +98,56 @@ void delEnd() {
         temp = temp->next;
     }
     printf("Deleted %s\n",temp->next->song);
-    free(temp->next);
     temp->next=NULL;
     free(temp);
     return;
 }
+
+void menu() {
+    printf("-----Song PlayList-------\n");
+    printf("1.Add Song\n");
+    printf("2.Play From Start\n");
+    printf("3.Play From End\n");
+    printf("4. Delete From Start\n");
+    printf("5. Delete From End\n");
+    printf("6. Exit\n");
+}
+
+
+int main() {
+    int choice;
+    while (1) {
+        menu();
+        printf("Enter your choice");
+        scanf("%d",&choice);
+        switch (choice) {
+            case 1:addSong(); break;
+                case 2:playStart(); break;
+                case 3:playEnd(); break;
+                case 4:delStart(); break;
+                case 5:delEnd(); break;
+                case 6:exit(0);
+                default:printf("Wrong choice\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
